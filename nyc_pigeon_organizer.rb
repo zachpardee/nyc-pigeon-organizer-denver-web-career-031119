6c1a99b2203f9b9890cd91f4cd91783fb0dbf08d
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 21:02:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each{|attribute, characteristic|
+  characteristic.each{|features, name|
+    name.each{|pigeon|
+      new_hash[pigeon] ||= {}
+      new_hash[pigeon][attribute] ||= []
+      new_hash[pigeon][attribute] << features.to_s}}}
+      new_hash
 end
